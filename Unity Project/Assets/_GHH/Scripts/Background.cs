4310368001e6b7fd4bd496478ffd4b025b92b676
ghh2013 <ghh2004@naver.com>
2020-06-08 13:58:16 +0900

    알파모델
    백그라운드 스크롤링
    라이커스

---
+++ Unity Project/Assets/_GHH/Scripts/Background.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    Material mat;
+    public float scrollSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackgroundScroll();
+    }
+
+    private void BackgroundScroll()
+    {
+        Vector2 offset = mat.mainTextureOffset;
+        //offset.y += scrollSpeed * Time.deltaTime;
+        offset.Set(0, offset.y + (scrollSpeed * Time.deltaTime));
+        mat.mainTextureOffset = offset;
+    }
+}
